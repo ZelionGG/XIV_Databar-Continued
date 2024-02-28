40bf47841fee718a7a63339dd30c34991305cc77
Aurélien ALVES <contact@zeliongg.com>
2024-02-28 11:10:55 +0100

    2.7.1 changelog added.

---
+++ Core/changelog/2.7.1.lua
@@ -0,0 +1,34 @@
+local AddOnName, XIVBar = ...;
+
+XIVBar.Changelog[2701] = {
+    version_string = "2.7.1",
+    release_date = "2024/02/28",
+    important = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "[Global] Fixed an addon breaking error caused by unmaintained locales in the [Changelog] module.",
+        },
+        ["frFR"] = {
+            "[Global] Correction d'une erreur critique causée par des langues non prises en charge dans le module [Changelog].",
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    new = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    improvment = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    }
+}
