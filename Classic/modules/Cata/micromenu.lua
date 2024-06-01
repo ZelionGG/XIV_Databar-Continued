36e215da2efb83b97201fe636111fcb5b9a157af
Aurélien ALVES <contact@zeliongg.com>
2024-06-02 00:14:08 +0200

    #26 - Fixed guild button in micromenu.

--- Classic/modules/Cata/micromenu.lua
+++ Classic/modules/Cata/micromenu.lua
@@ -938,7 +938,7 @@ function MenuModule:CreateClickFunctions()
             return;
         end
         if button == "LeftButton" then
-            ToggleFriendsFrame(3)
+            ToggleGuildFrame(0)
         end
     end; -- guild
 
