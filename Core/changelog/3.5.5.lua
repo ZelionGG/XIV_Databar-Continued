e07bbb54e44530327201043a68ed8dec2b517f26
ZelionGG <contact@zeliongg.com>
2025-04-23 12:38:23 +0200

    CHANGELOGs update.

---
+++ Core/changelog/3.5.5.lua
@@ -0,0 +1,38 @@
+local AddOnName, XIVBar = ...;
+
+XIVBar.Changelog[3550] = {
+    version_string = "3.5.5",
+    release_date = "2025/04/23",
+    important = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "[Retail] Updated for [11.1.5]."
+        },
+        ["frFR"] = {
+            "[Retail] Mis à jour pour la version [11.1.5]."
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    new = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "[Retail] Added 4 missing Hearthstones toys in the [Travel] module."
+        },
+        ["frFR"] = {
+            "[Retail] Ajout de 4 jouets de pierres de foyer manquants au module [Voyage]."
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    improvment = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    }
+}
