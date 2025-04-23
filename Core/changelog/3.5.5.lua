5d518bb2fa90636b7e1ff5a7b818b379b27ccdf5
ZelionGG <contact@zeliongg.com>
2025-04-23 17:34:08 +0200

    Syntax fixing.

--- Core/changelog/3.5.5.lua
+++ Core/changelog/3.5.5.lua
@@ -7,11 +7,11 @@ XIVBar.Changelog[3550] = {
         ["zhCN"] = {},
         ["zhTW"] = {},
         ["enUS"] = {
-            "[Retail] Updated for [11.1.5]."
+            "[Retail] Updated for [11.1.5].",
             "[Classic SoD] Updated for [1.15.7]."
         },
         ["frFR"] = {
-            "[Retail] Mis à jour pour la version [11.1.5]."
+            "[Retail] Mis à jour pour la version [11.1.5].",
             "[Classic SoD] Mis à jour pour la version [1.15.7]."
         },
         ["koKR"] = {},
@@ -21,10 +21,10 @@ XIVBar.Changelog[3550] = {
         ["zhCN"] = {},
         ["zhTW"] = {},
         ["enUS"] = {
-            "[Retail] Added 4 missing Hearthstones toys in the [Travel] module."
+            "[Retail] Added 4 missing Hearthstones toys in the [Travel] module.",
         },
         ["frFR"] = {
-            "[Retail] Ajout de 4 jouets de pierres de foyer manquants au module [Voyage]."
+            "[Retail] Ajout de 4 jouets de pierres de foyer manquants au module [Voyage].",
         },
         ["koKR"] = {},
         ["ruRU"] = {}
