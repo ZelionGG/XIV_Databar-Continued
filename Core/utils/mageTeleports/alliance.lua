341898fdac2c7c469218891e77195a17fa702181
ZelionGG <contact@zeliongg.com>
2025-06-10 21:23:07 +0200

    #69 - Added teleports files.

---
+++ Core/utils/mageTeleports/alliance.lua
@@ -0,0 +1,116 @@
+local AddOnName, xb = ...
+local L = xb.L
+
+-- Battle for Azeroth mythic teleports data
+xb.MageTeleports = xb.MageTeleports or {}
+xb.MageTeleports.alliance = {
+    name = L["Mage Teleports"],
+    teleports = {
+        {
+            teleportId = 3561, -- Stormwind
+            areaId = 1519,
+        },
+        {
+            teleportId = 3562, -- Ironforge
+            areaId = 1537,
+        },
+        {
+            teleportId = 3565, -- Darnassus
+            areaId = 1657,
+        },
+        {
+            teleportId = 32271, -- Exodar
+            areaId = 3557,
+        },
+        {
+            teleportId = 49359, -- Theramore
+            areaId = 513,
+        },
+        {
+            teleportId = 33690, -- Shattrath
+            areaId = 3703,
+        },
+        {
+            teleportId = 53140, -- Dalaran, Northrend
+            areaId = 8474,
+        },
+        {
+            teleportId = 88342, -- Tol Barad
+            areaId = 5095,
+        },
+        {
+            teleportId = 132621, -- Vale of Eternal Blossoms
+            areaId = 5840,
+        },
+        {
+            teleportId = 176248, -- Stormshield
+            areaId = 7332,
+        },
+        {
+            teleportId = 224869, -- Dalaran, Broken Isles
+            areaId = 7881,
+        },
+        {
+            teleportId = 281403, -- Boralus
+            areaId = 8568,
+        },
+        {
+            teleportId = 344587, -- Oribos
+            areaId = 10565,
+        },
+    },
+    portals = {
+        {
+            teleportId = 10059, -- Stormwind
+            areaId = 1519,
+        },
+        {
+            teleportId = 11416, -- Ironforge
+            areaId = 1537,
+        },
+        {
+            teleportId = 11419, -- Darnassus
+            areaId = 1657,
+        },
+        {
+            teleportId = 32266, -- Exodar
+            areaId = 3557,
+        },
+        {
+            teleportId = 49360, -- Theramore
+            areaId = 513,
+        },
+        {
+            teleportId = 33691, -- Shattrath
+            areaId = 3703,
+        },
+        {
+            teleportId = 53142, -- Dalaran, Northrend
+            areaId = 8474,
+        },
+        {
+            teleportId = 88345, -- Tol Barad
+            areaId = 5095,
+        },
+        {
+            teleportId = 132620, -- Vale of Eternal Blossoms
+            areaId = 5840,
+        },
+        {
+            teleportId = 176246, -- Stormshield
+            areaId = 7332,
+        },
+        {
+            teleportId = 224871, -- Dalaran, Broken Isles
+            areaId = 7881,
+        },
+        {
+            teleportId = 281400, -- Boralus
+            areaId = 8568,
+        },
+        {
+            teleportId = 344597, -- Oribos
+            areaId = 10565,
+        },
+    }
+}
---
+++ Core/utils/mageTeleports/horde.lua
@@ -0,0 +1,116 @@
+local AddOnName, xb = ...
+local L = xb.L
+
+-- Battle for Azeroth mythic teleports data
+xb.MageTeleports = xb.MageTeleports or {}
+xb.MageTeleports.horde = {
+    name = L["Mage Teleports"],
+    teleports = {
+        {
+            teleportId = 3567, -- Orgrimmar
+            areaId = 1637,
+        },
+        {
+            teleportId = 3563, -- Undercity
+            areaId = 1497,
+        },
+        {
+            teleportId = 3566, -- Thunder Bluff
+            areaId = 1638,
+        },
+        {
+            teleportId = 32272, -- Silvermoon
+            areaId = 3487,
+        },
+        {
+            teleportId = 49358, -- Stonard
+            areaId = 75,
+        },
+        {
+            teleportId = 35715, -- Shattrath
+            areaId = 3703,
+        },
+        {
+            teleportId = 53140, -- Dalaran, Northrend
+            areaId = 8474,
+        },
+        {
+            teleportId = 88344, -- Tol Barad
+            areaId = 5095,
+        },
+        {
+            teleportId = 132627, -- Vale of Eternal Blossoms
+            areaId = 5840,
+        },
+        {
+            teleportId = 176242, -- Warspear
+            areaId = 7333,
+        },
+        {
+            teleportId = 224869, -- Dalaran, Broken Isles
+            areaId = 7881,
+        },
+        {
+            teleportId = 281404, -- Dazar'alor
+            areaId = 8670,
+        },
+        {
+            teleportId = 344587, -- Oribos
+            areaId = 10565,
+        },
+    },
+    portals = {
+        {
+            teleportId = 3567, -- Orgrimmar
+            areaId = 1637,
+        },
+        {
+            teleportId = 3563, -- Undercity
+            areaId = 1497,
+        },
+        {
+            teleportId = 3566, -- Thunder Bluff
+            areaId = 1638,
+        },
+        {
+            teleportId = 32272, -- Silvermoon
+            areaId = 3487,
+        },
+        {
+            teleportId = 49358, -- Stonard
+            areaId = 75,
+        },
+        {
+            teleportId = 35715, -- Shattrath
+            areaId = 3703,
+        },
+        {
+            teleportId = 53140, -- Dalaran, Northrend
+            areaId = 8474,
+        },
+        {
+            teleportId = 88344, -- Tol Barad
+            areaId = 5095,
+        },
+        {
+            teleportId = 132627, -- Vale of Eternal Blossoms
+            areaId = 5840,
+        },
+        {
+            teleportId = 176242, -- Warspear
+            areaId = 7333,
+        },
+        {
+            teleportId = 224869, -- Dalaran, Broken Isles
+            areaId = 7881,
+        },
+        {
+            teleportId = 281404, -- Dazar'alor
+            areaId = 8670,
+        },
+        {
+            teleportId = 344587, -- Oribos
+            areaId = 10565,
+        },
+    }
+}
