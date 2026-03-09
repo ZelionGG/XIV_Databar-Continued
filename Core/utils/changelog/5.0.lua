ae4552b925a06304c0ea7cf385597b9448db9d24
ZelionGG <contact@zeliongg.com>
2026-03-09 23:41:06 +0100

    CHANGELOG prep.

---
+++ Core/utils/changelog/5.0.lua
@@ -0,0 +1,45 @@
+---@class XIVBar
+local XIVBar = select(2, ...);
+
+XIVBar.Changelog[5000] = {
+    version_string = "5.0",
+    release_date = "2026/03/09",
+    important = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "|TInterface\\OptionsFrame\\UI-OptionsFrame-NewFeatureIcon:16:16:0:0|t |cffffd700 Modules Free Positioning feature|r\n\n" ..
+            "After years of waiting, the dream is real: you can now position modules wherever you want! Enable [Module Positioning] in the settings, slide each module along the X-axis with the new controls, let the addon auto-capture your current layout, and enjoy free, precise placement without breaking your legacy setup."
+        },
+        ["frFR"] = {
+            "|TInterface\\OptionsFrame\\UI-OptionsFrame-NewFeatureIcon:16:16:0:0|t |cffffd700 Fonctionnalité de placement libre des modules|r\n\n" ..
+            "Après des années d'attente, le rêve est devenu réalité : vous pouvez désormais positionner les modules où vous le souhaitez ! Activez [Module Positioning] dans les paramètres, faites glisser chaque module sur l'axe X avec les nouveaux contrôles, laissez l'addon capturer automatiquement votre disposition actuelle, et profitez d'un placement libre et précis sans casser votre ancienne configuration."
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    new = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    improvment = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    bugfix = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    }
+}
