3587c3891a0ca8dd8645284659bb7c9c0f82f94b
ZelionGG <contact@zeliongg.com>
2026-03-12 12:22:42 +0100

    CHANGELOGs update

---
+++ Core/utils/changelog/4.8.2.lua
@@ -0,0 +1,43 @@
+---@class XIVBar
+local XIVBar = select(2, ...);
+
+XIVBar.Changelog[4820] = {
+    version_string = "4.8.2",
+    release_date = "2026/03/12",
+    important = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    new = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    improvment = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {},
+        ["frFR"] = {},
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    },
+    bugfix = {
+        ["zhCN"] = {},
+        ["zhTW"] = {},
+        ["enUS"] = {
+            "[Retail] Fixed the Micromenu LUA errors, disabling [Blizzard MicroMenu] from XIV is now blocked if a [Bar Manager] AddOn (ElvUI, Tukui, Bartender4, Dominos, etc.) is loaded.",
+        },
+        ["frFR"] = {
+            "[Retail] Correction des erreurs LUA du Micromenu, la désactivation du [MicroMenu Blizzard] est maintenant bloquée dans XIV si un AddOn de [Gestion des barres d'actions] (ElvUI, Tukui, Bartender4, Dominos, etc.) est chargé.",
+        },
+        ["koKR"] = {},
+        ["ruRU"] = {}
+    }
+}
