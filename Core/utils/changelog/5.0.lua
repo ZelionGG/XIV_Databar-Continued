667642f58558dff0f3f177c1267622609c0bd1ff
ZelionGG <contact@zeliongg.com>
2026-03-13 21:04:57 +0100

    5.0 Changelog updated

--- Core/utils/changelog/5.0.lua
+++ Core/utils/changelog/5.0.lua
@@ -9,11 +9,11 @@ XIVBar.Changelog[5000] = {
         ["zhTW"] = {},
         ["enUS"] = {
             "|TInterface\\OptionsFrame\\UI-OptionsFrame-NewFeatureIcon:16:16:0:0|t |cffffd700 Modules Free Positioning feature|r\n\n" ..
-            "After years of waiting, the dream is real: you can now position modules wherever you want! Enable [Module Positioning] in the settings, slide each module along the X-axis with the new controls, let the addon auto-capture your current layout, and enjoy free, precise placement without breaking your legacy setup."
+            "After years of waiting, the dream is real: you can now position modules however you want! Enable [Module Positioning] in the settings, slide each module along the X-axis with the new controls, let the addon auto-capture your current layout, and enjoy free, precise placement without breaking your legacy setup."
         },
         ["frFR"] = {
             "|TInterface\\OptionsFrame\\UI-OptionsFrame-NewFeatureIcon:16:16:0:0|t |cffffd700 Fonctionnalité de placement libre des modules|r\n\n" ..
-            "Après des années d'attente, le rêve est devenu réalité : vous pouvez désormais positionner les modules où vous le souhaitez ! Activez [Module Positioning] dans les paramètres, faites glisser chaque module sur l'axe X avec les nouveaux contrôles, laissez l'addon capturer automatiquement votre disposition actuelle, et profitez d'un placement libre et précis sans casser votre ancienne configuration."
+            "Après des années d'attente, le rêve est devenu réalité : vous pouvez désormais positionner les modules comme vous le souhaitez ! Activez [Module Positioning] dans les paramètres, faites glisser chaque module sur l'axe X avec les nouveaux contrôles, laissez l'addon capturer automatiquement votre disposition actuelle, et profitez d'un placement libre et précis sans casser votre ancienne configuration."
         },
         ["koKR"] = {},
         ["ruRU"] = {}
@@ -29,7 +29,12 @@ XIVBar.Changelog[5000] = {
     improvment = {
         ["zhCN"] = {},
         ["zhTW"] = {},
-        ["enUS"] = {},
+        ["enUS"] = {
+            "[Retail] The [Great Vault] click is now disabled until the next season starts and a disclaimer was added in the tooltip.",
+            "[Retail] The [Great Vault] won't show anymore if you're not at max level.",
+            "[Retail] Added a disclaimer in the [Great Vault] module options to warn that max level is needed for the module to show.",
+            '[Global] Options for hiding Blizzard UI in the [Micromenu] and [Gold] modules when external action bar AddOns are detected will now be disabled.'
+        },
         ["frFR"] = {},
         ["koKR"] = {},
         ["ruRU"] = {}
