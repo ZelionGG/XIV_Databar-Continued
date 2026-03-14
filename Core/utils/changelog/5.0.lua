a963458d9608eb2e4be71837e03294be94411b7b
ZelionGG <contact@zeliongg.com>
2026-03-14 12:26:28 +0100

    5.0 Changelog updated

--- Core/utils/changelog/5.0.lua
+++ Core/utils/changelog/5.0.lua
@@ -23,11 +23,13 @@ XIVBar.Changelog[5000] = {
         ["zhTW"] = {},
         ["enUS"] = {
             "[Global] Added a new highly customizable [Rest Icon] in the [Clock] module (Thank you [Skyfox] on the Discord for the suggestion).",
-            "[Global] Added an option to allow setting sound volume from the [Master Volume] module using mouse wheel (Thank you [Skyfox] on the Discord for the suggestion)."
+            "[Global] Added an option to allow setting sound volume from the [Master Volume] module using mouse wheel (Thank you [Skyfox] on the Discord for the suggestion).",
+            "[Global] It is now possible to hide the [Clock] module when [Module Free Placement] is enabled, if [Module Free Placement] is disabled, the [Clock] module will show again automatically."
         },
         ["frFR"] = {
             "[Global] Ajout d'une nouvelle icône de repos hautement personnalisable dans le module [Horloge] (Merci à [Skyfox] sur le Discord pour la suggestion).",
-            "[Global] Ajout d'une option pour permettre de régler le volume sonore depuis le module [Master Volume] en utilisant la molette de la souris (Merci à [Skyfox] sur le Discord pour la suggestion)."
+            "[Global] Ajout d'une option pour permettre de régler le volume sonore depuis le module [Master Volume] en utilisant la molette de la souris (Merci à [Skyfox] sur le Discord pour la suggestion).",
+            "[Global] Le module [Horloge] peut maintenant être masqué quand le [Placement libre des modules] est activé, s'il est désactivé, le module [Horloge] sera affiché à nouveau automatiquement."
         },
         ["koKR"] = {},
         ["ruRU"] = {}
@@ -40,13 +42,17 @@ XIVBar.Changelog[5000] = {
             "[Retail] The [Great Vault] won't show anymore if your character is not at max level.",
             "[Retail] Added a disclaimer in the [Great Vault] module options to warn that max level is needed for the module to show.",
             "[Global] Options to hide Blizzard UI in the [Micromenu] and [" .. BONUS_ROLL_REWARD_MONEY .. "] modules are now disabled when an external action bar AddOn is detected.",
-            "[Code] Refactored the locales system to make it easier to maintain, read, and update."
+            "[Global] Popups now stay visible when using [Show on mouseover].",
+            "[Global] [Talent] and [Travel] popups are now closed automatically when you click anywhere outside of them.",
+            "[Code] Refactored the locales system to make it easier to maintain, read, and update.",
         },
         ["frFR"] = {
             "[Retail] Le clic sur [La Grande Chambre Forte] est maintenant désactivé jusqu'au début de la prochaine saison et un avertissement a été ajouté dans l'infobulle.",
             "[Retail] [La Grande Chambre Forte] ne s'affiche plus si votre personnage n'est pas au niveau maximum.",
             "[Retail] Un avertissement a été ajouté dans les options du module [La Grande Chambre Forte] pour prévenir que le niveau maximum est nécessaire pour que le module s'affiche.",
             "[Global] Les options pour masquer l'interface Blizzard dans les modules [Micro menu] et [" .. BONUS_ROLL_REWARD_MONEY .. "] sont maintenant désactivées quand un AddOn de barre d'action externe est détecté.",
+            "[Global] Les popups restent maintenant affichées correctement lorsque l’option [Afficher au survol] est active.",
+            "[Global] Les popups [Talents] et [Voyage] se ferment désormais automatiquement lorsque vous cliquez ailleurs sur l'écran.",
             "[Code] Le système de localisation a été repensé pour être plus facile à maintenir, à lire et à mettre à jour."
         },
         ["koKR"] = {},
